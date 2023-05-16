2a0407d65e3c45d8b8eac944969fc2003a7adb2a
JakubTlak <marbixxd@gmail.com>
2023-05-16 15:01:06 +0200

    display of pizzas and allergents

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,14 @@ const cors = require("cors")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors)
+app.use(cors());
 
 app.get("/", (req, res) => {
-	res	.sendFile(path.join(`${__dirname}/../frontend/index.html`));  
+	res.sendFile(path.join(`${__dirname}/../frontend/index.html`));  
 });
 
-app.get("/script.js", (req, res) => {
-	res.sendFile(path.join(`${__dirname}/../frontend/script.js`));  
-});
+app.use("/public", express.static(`${__dirname}/../frontend/public`))
 
 app.use("/api", apiRouter);
 
-app.listen(3000, () => console.log(`http://127.0.0.1:${3000}`)); 
+app.listen(3000,'127.0.0.1', () => console.log(`http://127.0.0.1:${3000}`)); 
