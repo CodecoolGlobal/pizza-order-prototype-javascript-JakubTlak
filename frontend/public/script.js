bb1ea1d7188f46edd329bab78eeae0f8381d19d5
SebastianKostrz <sebastian.kostrz02@gmail.com>
2023-05-17 10:53:30 +0200

    Add pizzas filtering by allergenns

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,10 +1,27 @@
-
+let currentPizzas;
 
 async function fetchingData(url) {
     const response = await fetch(url);
     const result = await response.json();
     console.log(result)
     return result;
+}
+
+function returnTemplate(pizza){
+    return `<div class="pizza" id="${'pizza:' + pizza.id}">
+    <div class="pizza-name" >
+    <h2>${pizza.name}</h2>
+    </div>
+    <div class="allergens" id="${pizza.allergens}"></div>
+    <img src=${pizza.img}>
+    <div class="ingedients">
+    <p>${pizza.ingedients}</p>
+    </div>
+    <div class="price">
+    <p>${pizza.price}zł</p>
+    </div>
+    </div>
+    `
 }
 
 async function displayPizzas() {
@@ -32,6 +49,8 @@ async function displayPizzas() {
 }
 
 
+
+
 async function displayAllergents() {
     const allergenData = await fetchingData("http://localhost:3000/api/allergen");
 
@@ -44,8 +63,7 @@ async function displayAllergents() {
     return text;
 }
 
-function load() {
-
+async function load() {
     const pizzasForm = document.getElementById("pizzas")
     const allergentsForm = document.getElementById('allergents')
 
@@ -66,17 +84,34 @@ function load() {
     const pizzaElements = document.querySelectorAll(".pizza")
     const allergenElements = document.querySelectorAll(".allergens")
 
-    window.addEventListener("change", function (event) {
-        checkboxElements.forEach(checkbox => {
-            pizzaElements.forEach(pizza => {
-                allergenElements.forEach(allergen => {
-                    if (checkbox.checked === true && allergen.id.includes(checkbox.id.split(":")[1])) {
-                        console.log("git");
-                        pizza.remove();
-                    }
-                })
+    // window.addEventListener("change", function (event) {
+    //     checkboxElements.forEach(checkbox => {
+    //         pizzaElements.forEach(pizza => {
+    //             allergenElements.forEach(allergen => {
+    //                 if (checkbox.checked === true && allergen.id.includes(checkbox.id.split(":")[1])) {
+    //                     console.log("git");
+    //                     pizza.remove();
+    //                 }
+    //             })
+    //         })
+    //     })
+    // })
+    window.addEventListener('change', async (e) => {
+        if (e.target.classList.contains('allergen-input')) {
+            const checkboxes = Array.from(document.getElementsByClassName('allergen-input'));
+            let pizzas = await fetchingData("http://localhost:3000/api/pizza");
+            checkboxes.forEach(box=>{
+                let id = box.id.split(":")[1];
+                if(box.checked==true){
+                    pizzas.pizzas=pizzas.pizzas.filter(pizza=>!pizza.allergens.includes(parseInt(id)));
+                }
             })
-        })
+            document.getElementById('pizzas').replaceChildren();
+            pizzas.pizzas.forEach(pizza=>{
+                document.getElementById('pizzas').insertAdjacentHTML('beforeend',returnTemplate(pizza));
+            })
+
+        }
     })
 }
 
